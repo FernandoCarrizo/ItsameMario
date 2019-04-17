c0971692b8e3f8e69928ed1618672c2ac8bb745b
TomaszKisiel <tomcio61@onet.pl>
2019-04-17 20:46:02 +0000

    Mario Ep14
    Improving the physics script and collision detection.

---
+++ js/poruszanie.js
@@ -0,0 +1,16 @@
+class Poruszanie {
+	aktualizacja(dane) {
+		this.mario(dane);
+		this.potwor(dane);
+	}
+	
+	mario(dane) {
+		dane.obiekty.mario.obecnyStan.ruch(dane);
+	}
+	
+	potwor(dane) {
+		dane.obiekty.tabelaPotworow.forEach((p) => {
+			p.obecnyStan.ruch(dane);
+		});
+	}
+}
