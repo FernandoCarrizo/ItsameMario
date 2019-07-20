d507971cec8e3ef8c0dadbac89b8e525251342be
EQSGit <46108552+EqsGIT@users.noreply.github.com>
2019-07-20 16:35:30 +0200

    .
    .

--- js/poruszanie.js
+++ js/poruszanie.js
@@ -1,16 +1,16 @@
 class Poruszanie {
-	aktualizacja(dane) {
+  aktualizacja(dane) {
 		this.mario(dane);
 		this.potwor(dane);
 	}
-	
-	mario(dane) {
-		dane.obiekty.mario.obecnyStan.ruch(dane);
-	}
-	
-	potwor(dane) {
-		dane.obiekty.tabelaPotworow.forEach((p) => {
-			p.obecnyStan.ruch(dane);
-		});
-	}
+
+  mario(dane) {
+    dane.obiekty.mario.obecnyStan.ruch(dane);
+  }
+
+  potwor(dane) {
+    dane.obiekty.tabelaPotworow.forEach(function(p) {
+      p.obecnyStan.ruch(dane);
+    });
+  }
 }
--- js/wejscie.js
+++ js/wejscie.js
@@ -1,21 +1,21 @@
 class Wejscie {
-	constructor(dane) {
-		this.nacisniety = {};
-		
-		document.onkeydown = (event) => {
+  constructor() {
+    this.nacisniety = {};
+    document.onkeydown = (event) => {
 			this.nacisniety[event.keyCode] = true;
 		}
-		
+
 		document.onkeyup = (event) => {
 			this.nacisniety[event.keyCode] = false;
 		}
-	}
-	
-	aktualizacja(dane) {
-		let mario = dane.obiekty.mario;
-		
+  }
+
+  aktualizacja(dane) {
+    let mario = dane.obiekty.mario;
+
 		if(this.nacisnieto(39)) {
 			mario.kierunek = "prawo";
+
 			if(mario.pedY == 0) {
 				mario.obecnyStan = mario.stan.poruszanie;
 			} else {
@@ -29,7 +29,7 @@ class Wejscie {
 					for( let i = 0; i<dane.obiekty.tabelaPotworow.length; i++) {
 						dane.obiekty.tabelaPotworow[i].x -= mario.pedX;
 					}
-					for( let i = 0; i<dane.obiekty.tabelaMonet.length; i++) {
+          for( let i = 0; i<dane.obiekty.tabelaMonet.length; i++) {
 						dane.obiekty.tabelaMonet[i].x -= mario.pedX;
 					}
 				}
@@ -37,7 +37,7 @@ class Wejscie {
 		}
 		if(this.nacisnieto(37)) {
 			mario.kierunek = "lewo";
-			
+
 			if(mario.pedY == 0) {
 				mario.obecnyStan = mario.stan.poruszanie;
 			} else {
@@ -51,18 +51,18 @@ class Wejscie {
 					for( let i = 0; i<dane.obiekty.tabelaPotworow.length; i++) {
 						dane.obiekty.tabelaPotworow[i].x += mario.pedX;
 					}
-					for( let i = 0; i<dane.obiekty.tabelaMonet.length; i++) {
+          for( let i = 0; i<dane.obiekty.tabelaMonet.length; i++) {
 						dane.obiekty.tabelaMonet[i].x += mario.pedX;
 					}
 				}
 			}
 		}
 		if(this.nacisnieto(32)) {
-			mario.obecnyStan = mario.stan.skakanie;      
+			mario.obecnyStan = mario.stan.skakanie;
 		}
-	}
-	
-	nacisnieto(kod) {
-		return this.nacisniety[kod];
-	}
+  }
+
+  nacisnieto(kod) {
+    return this.nacisniety[kod];
+  }
 }
