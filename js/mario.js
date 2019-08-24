d6bc22f29c2374c261968690cd11e2c42a785c45
EqsGIT <MalyProGra@gmail.com>
2019-08-24 20:53:20 +0000

    Mario_Ep19
    Mario_Ep19

--- js/mario.js
+++ js/mario.js
@@ -22,86 +22,16 @@ class Mario {
       skokLewo: new Obraz(img,3472,80,16,16),
       smierc: new Obraz(img, 3488, 64, 16, 16)
     };
-    this.animacjaDuzy = {
-      poruszaniePrawo: {
-        klatka: [new Obraz(img,3424,0,16,32),
-                 new Obraz(img,3408,0,16,32),
-                 new Obraz(img,3424,0,16,32),
-                 new Obraz(img,3440,0,16,32)],
-        obecnaKlatka: 0
-      },
-      poruszanieLewo: {
-        klatka: [new Obraz(img,3520,0,16,32),
-                 new Obraz(img,3504,0,16,32),
-                 new Obraz(img,3520,0,16,32),
-                 new Obraz(img,3536,0,16,32)],
-        obecnaKlatka: 0
-      },
-      miganiePrawo: {
-        klatka: [new Obraz(img,3488,0,16,32),
-                 new Obraz(img,3520,64,16,32)],
-        obecnaKlatka: 0
-      },
-      miganieLewo: {
-        klatka: [new Obraz(img,3584,0,16,32),
-                 new Obraz(img,3520,64,16,32)],
-        obecnaKlatka: 0
-      },
-      staniePrawo: new Obraz(img,3488,0,16,32),
-      stanieLewo: new Obraz(img,3584,0,16,32),
-      skokPrawo: new Obraz(img,3472,0,16,32),
-      skokLewo: new Obraz(img,3568,0,16,32),
-    };
-    this.animacjaStrzelanie = {
-      poruszaniePrawo: {
-        klatka: [new Obraz(img,3424,32,16,32),
-                 new Obraz(img,3408,32,16,32),
-                 new Obraz(img,3424,32,16,32),
-                 new Obraz(img,3440,32,16,32)],
-        obecnaKlatka: 0
-      },
-      poruszanieLewo: {
-        klatka: [new Obraz(img,3520,32,16,32),
-                 new Obraz(img,3504,32,16,32),
-                 new Obraz(img,3520,32,16,32),
-                 new Obraz(img,3536,32,16,32)],
-        obecnaKlatka: 0
-      },
-      miganiePrawo: {
-        klatka: [new Obraz(img,3488,32,16,32),
-                 new Obraz(img,3520,64,16,32)],
-        obecnaKlatka: 0
-      },
-      miganieLewo: {
-        klatka: [new Obraz(img,3584,32,16,32),
-                 new Obraz(img,3520,64,16,32)],
-        obecnaKlatka: 0
-      },
-      staniePrawo: new Obraz(img,3488,32,16,32),
-      stanieLewo: new Obraz(img,3584,32,16,32),
-      skokPrawo: new Obraz(img,3472,32,16,32),
-      skokLewo: new Obraz(img,3568,32,16,32),
-    };
     this.stan = {
       stanie: {
         ruch: (dane) => {
           return;
         },
         animacja: (dane) => {
-          let animacja = this.animacja;
-          if(this.mozeStrzelac) {
-            animacja = this.animacjaStrzelanie;
-            this.h = 2 * h - 1;
-          } else if(this.mozeNiszczyc) {
-            animacja = this.animacjaDuzy;
-            this.h = 2 * h - 1;
-          } else {
-            this.h = h;
-          }
           if(this.kierunek==="prawo") {
-            this.obraz = animacja.staniePrawo;
+            this.obraz = this.animacja.staniePrawo;
           } else {
-            this.obraz = animacja.stanieLewo;
+            this.obraz = this.animacja.stanieLewo;
           }
         }
       },
@@ -114,20 +44,10 @@ class Mario {
           this.kontrolerRuchu(dane);
         },
         animacja: (dane) => {
-          let animacja = this.animacja;
-          if(this.mozeStrzelac) {
-            animacja = this.animacjaStrzelanie;
-            this.h = 2 * h - 1;
-          } else if(this.mozeNiszczyc) {
-            animacja = this.animacjaDuzy;
-            this.h = 2 * h - 1;
-          } else {
-            this.h = h;
-          }
           if(this.kierunek==="prawo") {
-            this.obraz = animacja.skokPrawo;
+            this.obraz = this.animacja.skokPrawo;
           } else {
-            this.obraz = animacja.skokLewo;
+            this.obraz = this.animacja.skokLewo;
           }
         }
       },
@@ -136,67 +56,23 @@ class Mario {
           this.kontrolerRuchu(dane);
         },
         animacja: (dane) => {
-          let animacja = this.animacja;
-          if(this.mozeStrzelac) {
-            animacja = this.animacjaStrzelanie;
-            this.h = 2 * h - 1;
-          } else if(this.mozeNiszczyc) {
-            animacja = this.animacjaDuzy;
-            this.h = 2 * h - 1;
-          } else {
-            this.h = h;
-          }
           if(this.kierunek === "prawo") {
             if(dane.nrKlatki % 5 == 0) {
-              this.obraz = animacja.poruszaniePrawo.klatka[animacja.poruszaniePrawo.obecnaKlatka];
-              animacja.poruszaniePrawo.obecnaKlatka++;
+              this.obraz = this.animacja.poruszaniePrawo.klatka[this.animacja.poruszaniePrawo.obecnaKlatka];
+              this.animacja.poruszaniePrawo.obecnaKlatka++;
             }
 
-            if(animacja.poruszaniePrawo.obecnaKlatka>3) {
-              animacja.poruszaniePrawo.obecnaKlatka=0;
+            if(this.animacja.poruszaniePrawo.obecnaKlatka>3) {
+              this.animacja.poruszaniePrawo.obecnaKlatka=0;
             }
           } else {
             if(dane.nrKlatki % 5 == 0) {
-              this.obraz = animacja.poruszanieLewo.klatka[animacja.poruszanieLewo.obecnaKlatka];
-              animacja.poruszanieLewo.obecnaKlatka++;
+              this.obraz = this.animacja.poruszanieLewo.klatka[this.animacja.poruszanieLewo.obecnaKlatka];
+              this.animacja.poruszanieLewo.obecnaKlatka++;
             }
 
-            if(animacja.poruszanieLewo.obecnaKlatka>3) {
-              animacja.poruszanieLewo.obecnaKlatka=0;
-            }
-          }
-        }
-      },
-      miganie: {
-        licznik: 0,
-        ruch: (dane) => {
-          this.pedX = 0;
-          this.pedY = 0;
-        },
-        animacja: (dane) => {
-          let animacja = this.animacja;
-          if(this.mozeStrzelac) {
-            animacja = this.animacjaStrzelanie;
-          } else if(this.mozeNiszczyc) {
-            animacja = this.animacjaDuzy;
-          }
-          if(this.kierunek == "prawo") {
-            if(dane.nrKlatki % 5 == 0) {
-              this.obraz = animacja.miganiePrawo.klatka[animacja.miganiePrawo.obecnaKlatka];
-              animacja.miganiePrawo.obecnaKlatka++;
-            }
-
-            if(animacja.miganiePrawo.obecnaKlatka>1) {
-              animacja.miganiePrawo.obecnaKlatka=0;
-            }
-          } else {
-            if(dane.nrKlatki % 5 == 0) {
-              this.obraz = animacja.miganieLewo.klatka[animacja.miganieLewo.obecnaKlatka];
-              animacja.miganieLewo.obecnaKlatka++;
-            }
-
-            if(animacja.miganieLewo.obecnaKlatka>1) {
-              animacja.miganieLewo.obecnaKlatka=0;
+            if(this.animacja.poruszanieLewo.obecnaKlatka>3) {
+              this.animacja.poruszanieLewo.obecnaKlatka=0;
             }
           }
         }
@@ -206,7 +82,6 @@ class Mario {
           this.pedX = 0;
         },
         animacja: (dane) => {
-          this.h = h;
           this.obraz = this.animacja.smierc;
         }
       }
@@ -221,8 +96,6 @@ class Mario {
     this.pedX = 8;
     this.zycia = 3;
     this.mozeNiszczyc = false;
-    this.mozeStrzelac = false;
-    this.naladowany = true;
     this.momentSmierci = false;
     this.monety = 0;
     this.typ = "mario";
@@ -260,9 +133,6 @@ class Mario {
       }
       for( var i = 0; i<dane.obiekty.tabelaGrzybow.length; i++) {
         dane.obiekty.tabelaGrzybow[i].x -= this.pedX;
-      }
-      for( var i = 0; i<dane.obiekty.tabelaPociskow.length; i++) {
-        dane.obiekty.tabelaPociskow[i].x -= this.pedX;
       }
     }
   }
